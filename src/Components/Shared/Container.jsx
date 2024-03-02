58f66257c03b8c9e96d1fa4d7a8bb7471490a01a
tushar-454 <imtushar454@gmail.com>
2024-03-02 18:32:08 +0600

    Add Container component and update Header component

---
+++ src/Components/Shared/Container.jsx
@@ -0,0 +1,5 @@
+const Container = ({ children }) => {
+  return <div className='mx-auto max-w-screen-2xl px-4'>{children}</div>;
+};
+
+export default Container;
--- src/Components/Shared/Header.jsx
+++ src/Components/Shared/Header.jsx
@@ -1,10 +1,58 @@
+import { useState } from 'react';
+import { RxCross2 } from 'react-icons/rx';
+import { TiThMenu } from 'react-icons/ti';
+import { Link } from 'react-router-dom';
+import Container from './Container';
+
 const Header = () => {
+  const [showNav, setShowNav] = useState(false);
   return (
-    <div>
-      <h1 className='text-3xl font-bold underline underline-offset-4'>
-        Header
-      </h1>
-    </div>
+    <header>
+      <Container>
+        {/* wrapper for the header */}
+        <div className='relative my-2 flex items-center justify-between rounded-full bg-[#001F3F] p-1 px-5 text-white'>
+          {/* logo here */}
+          <div>
+            <span className='text-4xl font-black'>UK Logistic</span>
+          </div>
+          {/* nav show controller  */}
+          <div onClick={() => setShowNav(!showNav)} className='block lg:hidden'>
+            {showNav ? (
+              <RxCross2 className='cursor-pointer text-4xl' />
+            ) : (
+              <TiThMenu className='cursor-pointer text-4xl' />
+            )}
+          </div>
+          {/* nav here */}
+          <nav
+            className={`absolute right-0 top-16 origin-top scale-y-0 rounded-xl bg-blue-50 p-10 py-5 text-black transition-all lg:static lg:scale-y-100 lg:bg-transparent lg:p-0 lg:text-white ${
+              showNav && 'scale-y-100'
+            }`}
+          >
+            <ul className='flex flex-col gap-8 lg:flex-row'>
+              <li className='text-xl font-medium hover:underline'>
+                <Link to='/'>Home</Link>
+              </li>
+              <li className='text-xl font-medium hover:underline'>
+                <Link to='/'>About</Link>
+              </li>
+              <li className='text-xl font-medium hover:underline'>
+                <Link to='/'>Services</Link>
+              </li>
+              <li className='text-xl font-medium hover:underline'>
+                <Link to='/'>Products</Link>
+              </li>
+              <li className='text-xl font-medium hover:underline'>
+                <Link to='/'>Media</Link>
+              </li>
+              <li className='text-xl font-medium hover:underline'>
+                <Link to='/'>Contact</Link>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </Container>
+    </header>
   );
 };
 
